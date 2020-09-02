d96723e4aa20f8b332969276b920373de3e72a62
Valeria Szeremeta <valeriaszeremeta71@gmail.com>
2020-09-02 18:43:53 -0300

    front-end inicial e rotas

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require("body-parser")
 const app = express()
+const admin = require("./routes/admin")
+const path = require("path")
 // const moongoose = require("mongoose")
 // configurações
     // Body-Parser
@@ -12,9 +14,12 @@ const app = express()
     app.engine('handlebars', handlebars({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
     // Mongoose
+
+// Public
+    app.use(express.static(path.join(__dirname, "public")))
     
 // rotas
-
+    app.use('/admin', admin)
 
 // outros
 const PORT = 8081
